7766ffbfc93e69b7e9b702f0e37edecc4a9b23b8
Subhamdas20 <subham21.sd@gmail.com>
2021-10-29 19:03:26 +0530

    Added : uc2 to add contact in AddressBook

---
+++ src/com/bz/addressbook/Contacts.java
@@ -0,0 +1,32 @@
+package com.bz.addressbook;
+
+import java.util.Scanner;
+
+public class Contacts {
+        String firstName, lastName, address, city, state, emailId;
+        int zipCode, phoneNumber;
+        public Contacts(String firstName, String lastName, String address, String city, String state, String emailId, int zipCode, int phoneNumber) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.emailId = emailId;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+        }
+    public String toString () {
+        return "----------------------------------\n" +
+                "FirstName  =  " + firstName +
+                "\nLastName   =  " + lastName +
+                "\nAddress    =  " + address +
+                "\nCity       =  " + city +
+                "\nState      =  " + state +
+                "\nZipCode    =  " + zipCode +
+                "\nNumber     =  " + phoneNumber +
+                "\nEmail      =  " + emailId +
+                "\n-----------------------------------\n"
+                ;
+    }
+    }
+
